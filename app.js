6eae984b7c3f6210f2000220e906d2765a2d0a1e
GitDoIgote <igorsantosgomes312@gmail.com>
2024-09-06 10:58:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+function pesquisar(){
+    let resultados = ""
+
+    let section = document.getElementById("resultados-pesquisa");
+   
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if(campoPesquisa == ""){
+        section.innerHTML = "Nenhum jogador encontrado. Preencha o campo de busca"
+        return
+    }
+    campoPesquisa = campoPesquisa.toLowerCase();
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    for(let dado of dados){
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+resultados += `
+<div class="item-resultado">
+<h2><a href=${dado.link} target="_blank">${dado.titulo}</a></h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+</div>
+`
+        }
+    }
+
+    if(!resultados)
+        resultados = "<p>nada foi encontrado</p>"
+section.innerHTML = resultados
+}
+
+
