7b91cba040d19dd71c8c6382c4b7f73bcc352c52
GitDoIgote <igorsantosgomes312@gmail.com>
2024-09-06 10:35:25 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,122 @@
+let dados = [{
+    titulo: "Weverton",
+    descricao: "Weverton Pereira da Silva, goleiro do Palmeiras desde 2018, se destacou como um dos maiores da história do clube. Suas defesas decisivas foram cruciais na conquista de títulos como as Libertadores de 2020 e 2021. Além de suas habilidades técnicas, ele é um líder dentro de campo, inspirando confiança em seus companheiros.",
+    link: "https://pt.wikipedia.org/wiki/Weverton",
+    tags: "paredão, goleiro, liderança, decisivo"
+},{
+    titulo: "Murilo",
+    descricao: "Murilo Cerqueira, zagueiro do Palmeiras, é conhecido por sua solidez defensiva e jogo aéreo forte. Desde sua chegada, tem sido uma presença constante na defesa, contribuindo tanto na marcação quanto em jogadas de bola parada. Sua consistência ajudou o time a conquistar títulos importantes.",
+    link: "https://pt.wikipedia.org/wiki/Murilo_Cerqueira",
+    tags: "zagueiro, solidez, jogo, aéreo, consistência"
+},{
+    titulo: "Gustavo Gómez",
+    descricao: "Gustavo Gómez, capitão e zagueiro do Palmeiras, é uma referência de liderança e qualidade defensiva. Sua habilidade de antecipação e força física fazem dele um dos principais defensores da América do Sul, sendo fundamental nas conquistas recentes do clube.",
+    link: "https://pt.wikipedia.org/wiki/Gustavo_Gómez",
+    tags: "xerife, capitão, defensiva, liderança"
+},{
+    titulo: "Piquerez",
+    descricao: "Joaquín Piquerez, lateral-esquerdo do Palmeiras, destaca-se pela sua capacidade de apoiar o ataque e solidez defensiva. Com cruzamentos precisos e bom posicionamento, ele se tornou um dos principais jogadores na lateral do time, contribuindo para o sucesso recente do clube.",
+    link: "https://pt.wikipedia.org/wiki/Joaquín_Piquerez",
+    tags: "lateral esquerdo, apoio ao ataque, solidez, cruzamentos"
+},{
+    titulo: "Mayke",
+    descricao: "Mayke, lateral-direito do Palmeiras, é conhecido por sua velocidade e habilidade tanto na defesa quanto no ataque. Sua versatilidade e experiência foram cruciais em diversas conquistas do clube, sendo um jogador confiável em momentos decisivos.",
+    link: "https://pt.wikipedia.org/wiki/Mayke",
+    tags: "lateral direito, velocidade, versatilidade, experiência"
+},{
+    titulo: "Richard Ríos",
+    descricao: "Richard Ríos, volante do Palmeiras, é reconhecido por sua capacidade de marcação e qualidade na saída de bola. Com uma forte presença física e bom passe, ele se firmou como uma peça importante no meio-campo do time.",
+    link: "https://pt.wikipedia.org/wiki/Richard_Ríos",
+    tags: "rios, volante, marcação, qualidade na saída de bola, colombiano"
+},{
+    titulo: "Zé Rafael",
+    descricao: "Zé Rafael, meio-campista do Palmeiras, é conhecido por sua versatilidade e capacidade de transição entre defesa e ataque. Com um chute potente e forte presença física, ele se tornou um jogador chave nas conquistas recentes do clube.",
+    link: "https://pt.wikipedia.org/wiki/Zé_Rafael",
+    tags: "ze, meio-campista, versatilidade, transição, potência"
+},{
+    titulo: "Aníbal Moreno",
+    descricao: "Aníbal Moreno, meio-campista do Palmeiras, é valorizado por sua visão de jogo e habilidade no passe. Sua capacidade de controlar o ritmo da partida o torna um jogador importante na articulação das jogadas do time.",
+    link: "https://pt.wikipedia.org/wiki/Aníbal_Moreno",
+    tags: "meio-campista, visão de jogo, passe, articulação"
+},{
+    titulo: "Raphael Veiga",
+    descricao: "Raphael Veiga, meio-campista do Palmeiras, é um dos principais criadores de jogadas do time, conhecido por sua precisão nos passes e nas finalizações. Sua capacidade de decidir jogos importantes fez dele uma das estrelas do clube nas últimas temporadas.",
+    link: "https://pt.wikipedia.org/wiki/Raphael_Veiga",
+    tags: "meiocampista, criatividade, finalização, precisão"
+},{
+    titulo: "Estevão",
+    descricao: "Estevão, jovem promessa do Palmeiras, tem mostrado grande potencial com sua habilidade no drible e visão de jogo. Mesmo com pouca idade, já demonstrou maturidade em campo, sendo apontado como um dos futuros talentos do clube.",
+    link: "https://pt.wikipedia.org/wiki/Estevão_William_Sousa_de_Almeida",
+    tags: "jovem, promessa, drible, visão de jogo, talento"
+},{
+    titulo: "Flaco López",
+    descricao: "Flaco López, atacante do Palmeiras, é reconhecido por sua presença física e capacidade de finalizar com precisão. Sua força no ataque e habilidade em jogadas aéreas o tornam uma ameaça constante para as defesas adversárias.",
+    link: "https://pt.wikipedia.org/wiki/José_Manuel_López",
+    tags: "lopez, atacante, finalização, presença física, jogos aéreos"
+},{
+    titulo: "Felipe Anderson",
+    descricao: "Felipe Anderson, meio-campista ofensivo do Palmeiras, se destaca por sua habilidade no drible e capacidade de criar oportunidades de gol. Com experiência internacional, ele trouxe dinamismo e criatividade ao setor ofensivo do time.",
+    link: "https://pt.wikipedia.org/wiki/Felipe_Anderson",
+    tags: " meio-campista ofensivo,  drible,  criatividade,  experiência"
+},{
+    titulo: "Vitor Reis",
+    descricao: "Vitor Reis, zagueiro do Palmeiras, é uma jovem promessa que se destaca pela sua força física e capacidade de leitura de jogo. Ele vem ganhando espaço na defesa, mostrando maturidade e solidez nas partidas em que participa.",
+    link: "https://pt.wikipedia.org/wiki/Vitor_Reis",
+    tags: " zagueiro,  jovem, promessa,  forçafísica,  leituradejogo"
+},{
+    titulo: "Vanderlan",
+    descricao: "Vanderlan, lateral-esquerdo do Palmeiras, é conhecido por sua velocidade e capacidade de apoiar o ataque. Sua versatilidade e dedicação tática fazem dele uma opção confiável tanto na defesa quanto no setor ofensivo.",
+    link: "https://pt.wikipedia.org/wiki/Vanderlan_Barbosa_da_Silva",
+    tags: " lateral esquerdo,  velocidade,  apoio ao ataque,  versatilidade"
+},{
+    titulo: "Giay",
+    descricao: "Matías Giay, lateral-direito do Palmeiras, é reconhecido por sua habilidade no apoio ao ataque e solidez defensiva. Com boa técnica e visão de jogo, ele rapidamente se adaptou ao time e vem se destacando em sua posição.",
+    link: "https://pt.wikipedia.org/wiki/Matías_Giay",
+    tags: " lateral direito,  técnica,  apoio ao ataque,  solidez defensiva"
+},{
+    titulo: "Marcos Rocha",
+    descricao: "Marcos Rocha, lateral-direito experiente do Palmeiras, é conhecido por sua consistência defensiva e qualidade no apoio ao ataque. Com passagens vitoriosas por outros clubes, sua experiência tem sido fundamental nas conquistas recentes do Palmeiras.",
+    link: "https://pt.wikipedia.org/wiki/Marcos_Rocha",
+    tags: " lateral direito,  consistência,  experiência,  apoio ao ataque"
+},{
+    titulo: "Fabinho",
+    descricao: "Fabinho, volante do Palmeiras, é valorizado por sua capacidade de marcação e versatilidade no meio-campo. Sua disposição em campo e qualidade no passe o tornaram uma peça importante no elenco, especialmente em jogos de alta intensidade.",
+    link: "https://pt.wikipedia.org/wiki/Fabinho",
+    tags: " volante,  marcação,  versatilidade,  qualidade no passe"
+},{
+    titulo: "Lázaro",
+    descricao: "Lázaro, meio-campista do Palmeiras, é uma jovem promessa que se destaca pela sua habilidade no controle de bola e visão de jogo. Seu talento e potencial o colocam como uma das apostas para o futuro do clube.",
+    link: "https://pt.wikipedia.org/wiki/Lázaro",
+    tags: " lazaro,  jovem, promessa  controle de bola,  visão de jogo,  talento"
+},{
+    titulo: "Maurício",
+    descricao: "Maurício, atacante do Palmeiras, é conhecido por sua velocidade e capacidade de finalização. Seu dinamismo e agressividade no ataque o tornaram uma arma importante para o time, especialmente em jogadas de contra-ataque.",
+    link: "https://pt.wikipedia.org/wiki/Maurício",
+    tags: " mauricio,  atacante,  velocidade,  finalização,  dinamismo"
+},{
+    titulo: "Rony",
+    descricao: "Rony, atacante do Palmeiras, é conhecido por sua velocidade, habilidade no drible e determinação em campo. Sua entrega e capacidade de marcar gols decisivos fizeram dele um dos jogadores mais importantes do elenco, sendo fundamental em diversas conquistas do clube.",
+    link: "https://pt.wikipedia.org/wiki/Rony",
+    tags: "rustico, rony,  atacante,  velocidade,  drible,  decisivo"
+},{
+titulo: "Dudu",
+descricao: "Dudu, atacante do Palmeiras, é uma das principais estrelas do clube, conhecido por sua velocidade, dribles rápidos e habilidade de criação. Desde sua chegada, tem sido crucial em inúmeras conquistas, tornando-se um ídolo da torcida por suas atuações decisivas e gols importantes.",
+link: "https://pt.wikipedia.org/wiki/Dudu_(futebolista,_nascido_em_1992)",
+tags: " dudu,  atacante,  velocidade,  drible,  ídolo"
+},{
+titulo: "Caio Paulista",
+descricao: "Caio Paulista, lateral-esquerdo do Palmeiras, destaca-se por sua versatilidade e capacidade de atuar tanto pelas pontas quanto pelo centro do ataque. Com um estilo de jogo dinâmico, ele contribui com sua velocidade e movimentação, sendo uma opção importante no elenco do clube.",
+link: "https://pt.wikipedia.org/wiki/Caio_Paulista",
+tags: " lateral esquerdo,  versatilidade,  dinamismo,  movimentação"
+},{
+titulo: "Marcelo Lomba",
+descricao: "Marcelo Lomba, goleiro do Palmeiras, é conhecido por sua experiência e segurança sob as traves. Com passagens por grandes clubes, ele trouxe sua bagagem para o Palmeiras, onde tem sido uma opção confiável, especialmente em jogos de alta pressão.",
+link: "https://pt.wikipedia.org/wiki/Marcelo_Lomba",
+tags: " goleiro,  experiência,  segurança,  consistência"
+},{
+titulo: "Abel Ferreira",
+descricao: "Abel Ferreira, técnico do Palmeiras, é amplamente reconhecido por sua inteligência tática e capacidade de motivar seus jogadores. Desde que assumiu o comando, conduziu o time a inúmeras conquistas importantes, incluindo títulos de Libertadores, consolidando-se como um dos maiores treinadores da história do clube.",
+link: "https://pt.wikipedia.org/wiki/Abel_Ferreira",
+tags: " técnico,  inteligência tática,  motivação,  conquistas,  liderança"
+}];
+
